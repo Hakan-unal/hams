e3a974b242d54e6f0b02ac9bfa426b3ecaa99b93
Con Sınov <hakanunal_1996@hotmail.com>
2023-07-07 23:26:52 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,91 @@
-import { useState, useEffect } from "react";
-import { Row, Col, Select, Form, Button, Card, Space, Table } from 'antd';
-import { days, months, horoscope } from "./staticData/data"
+import { useState } from "react";
+import { Row, Col, Progress, Select, Space, DatePicker, Form, Button } from 'antd';
 import { showNotification } from "./components/general/notification";
-import Meta from "antd/es/card/Meta";
-import supabase from "../src/supabaseConfig";
-import { Auth } from '@supabase/auth-ui-react'
+import moment from "moment"
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Description',
-    dataIndex: 'description',
-    key: 'description',
-  },
-  {
-    title: 'Image',
-    dataIndex: 'image',
-    key: 'image',
-  },
-];
+type FormValues = {
+  type: string,
+  date: string,
+}
+
 
 
 const App = () => {
+  const [value, setValue] = useState<number | null>(null)
   const [form] = Form.useForm();
-  const [current, setCurrent] = useState<any>(horoscope[6])
-  const [fishes, setFishes] = useState<any>([])
-  const [session, setSession] = useState<any>(null)
 
 
-  const onFinish = (values: any) => {
-    calculator(values.day, values.month)
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    errorInfo.errorFields.map((o: any) => {
-      showNotification("error", "Form error", o.errors[0] + " !", null)
-
-    })
-  };
-
-
-  const calculator = (day: number, month: number) => {
-    if ((day >= 21 && month === 3) || (day <= 20 && month === 4)) setCurrent(horoscope[1])
-    if ((day >= 21 && month === 4) || (day <= 20 && month === 5)) setCurrent(horoscope[2])
-    if ((day >= 21 && month === 5) || (day <= 20 && month === 6)) setCurrent(horoscope[3])
-    if ((day >= 21 && month === 6) || (day <= 22 && month === 7)) setCurrent(horoscope[4])
-    if ((day >= 23 && month === 7) || (day <= 22 && month === 8)) setCurrent(horoscope[5])
-    if ((day >= 23 && month === 8) || (day <= 22 && month === 9)) setCurrent(horoscope[6])
-    if ((day >= 23 && month === 9) || (day <= 22 && month === 10)) setCurrent(horoscope[7])
-    if ((day >= 23 && month === 10) || (day <= 22 && month === 11)) setCurrent(horoscope[8])
-    if ((day >= 23 && month === 11) || (day <= 21 && month === 12)) setCurrent(horoscope[9])
-    if ((day >= 22 && month === 12) || (day <= 19 && month === 1)) setCurrent(horoscope[10])
-    if ((day >= 20 && month === 1) || (day <= 18 && month === 2)) setCurrent(horoscope[11])
-    if ((day >= 19 && month === 2) || (day <= 20 && month === 3)) setCurrent(horoscope[0])
+  const calculator = (values: any) => {
+    const today = moment(new Date())
+    const duration = moment.duration(today.diff(values.date.$d));
+    const hours = duration.asHours().toFixed(2);
+    const tempResult = parseInt(hours)
+    if (tempResult > 0) {
+      const val = tempResult / 24
+      const result = val / values.type * 100
+      setValue(result)
+    }
   }
 
 
-  // useEffect(() => {
-  //   getCountries();
-  // }, []);
 
+  const onFinish = (values: FormValues) => {
+    calculator(values)
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+    form.resetFields()
+  };
 
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-
-    return () => subscription.unsubscribe()
-  }, [])
-
-  useEffect(() => {
-    console.log(session)
-    session && getFishes()
-  }, [session])
-
-  const getFishes = async () => {
-    const { data } = await supabase.from("fish").select("*");
-    setFishes(data);
-  }
-
+  const onFinishFailed = () => {
+    showNotification("error", "Uyarı", "Hop hemşerim nereye formda eksik alanlar var", null)
+  };
 
   return (<Row>
-    <Col xs={24} sm={{ span: 8, offset: 8 }}>
-      <Table dataSource={fishes} columns={columns} />;
+    <Col xs={{ span: 21, offset: 1 }} sm={{ span: 4, offset: 10 }}>
+      <Space direction="vertical">
+        {value && <Progress type="circle" strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }} percent={value} />}
+
+        <Form
+          layout="vertical"
+          form={form}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            name="type"
+            label="Tür"
+            rules={[{ required: true, message: 'Alan boş bırakılamaz' }]}
+          >
+            <Select
+              style={{ width: "100%" }}
+              placeholder="Seçiniz"
+              options={[
+                { value: 30, label: "Bedelli" },
+                { value: 180, label: "Kısa" },
+                { value: 181, label: "Uzun" },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item
+            name="date"
+            label="Başlangıç Tarihi"
+            rules={[{ required: true, message: 'Alan boş bırakılamaz' }]}
+          >
+            <DatePicker format={"YYYY-MM-DD"} placeholder="Seçiniz" />
+
+          </Form.Item>
+
+          <Form.Item >
+            <Button block type="primary" htmlType="submit">
+              Hesapla
+            </Button>
+          </Form.Item>
+        </Form>
 
 
+
+      </Space>
     </Col>
-
   </Row>
   )
 }
